e81877b562e22835eacd68016c6c9b527e57c3e6
Carlos Adrián López <97137104+Charly94630@users.noreply.github.com>
2024-01-19 01:39:58 -0300

    Update installjenkins.sh
    modified script for ubuntu

--- scripts/installjenkins.sh
+++ scripts/installjenkins.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-sudo yum update –y
-sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-sudo yum upgrade -y
-sudo dnf install java-17-amazon-corretto -y
-sudo yum install jenkins -y
+sudo apt update -y
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install jenkins -y
+sudo apt install fontconfig openjdk-17-jre -y
 sudo systemctl enable jenkins
-sudo systemctl start jenkinsç
 jenkinsPath="/var/lib/jenkins/secrets/initialAdminPassword"
 if [-e $jenkinsPath ]; then
 sudo cat $jenkinsPath
